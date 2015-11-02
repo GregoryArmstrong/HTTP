e3ccaabdd82cc88a080d1ffdecacbad7e87aa76f
GregoryArmstrong <intergalacticgentleman@gmail.com>
2015-11-02 13:53:38 -0700

    started testing file, server currently works

--- http_experiment.rb
+++ http_experiment.rb
@@ -1,41 +1,43 @@
 require 'socket'
-tcp_server = TCPServer.new(9292)
-client = tcp_server.accept
-request_counter = 0
+
+class Experiment
+
+  tcp_server = TCPServer.new(9292)
+  client = tcp_server.accept
+  request_counter = 0
+
+  loop do
+    puts "Ready for a request"
+    request_lines = []
+    while line = client.gets and !line.chomp.empty?
+      request_lines << line.chomp
+    end
+    request_counter += 1
+
+    puts "Got this request:"
+    puts request_lines.inspect
 
 
-loop do
-  puts "Ready for a request"
-  request_lines = []
-  while line = client.gets and !line.chomp.empty?
-    request_lines << line.chomp
+    puts "Sending response."
+    response = "<pre>" + request_lines.join("\n") + "</pre>"
+    response_2 = "<p>Hello, World! (#{request_counter})</p>"
+
+    output = "<html><head></head><body>#{response}#{response_2}</body></html>"
+    headers = ["http/1.1 200 ok",
+              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+              "server: ruby",
+              "content-type: text/html; charset=iso-8859-1",
+              "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    client.puts headers
+    client.puts output
+
+    puts ["Wrote this response:", headers, output].join("\n")
+
+    puts "Enter quit to stop server:"
+    closer = gets.chomp
+    break if closer == "quit"
   end
-  request_counter += 1
 
-  puts "Got this request:"
-  puts request_lines.inspect
+  puts "\nResponse complete, exiting."
 
-
-  puts "Sending response."
-  response = "<pre>" + request_lines.join("\n") + "</pre>"
-  response2 = "<p>Hello, World! (#{request_counter})</p>"
-  output = "<html><head></head><body>#{response}#{response2}</body></html>"
-  headers = ["http/1.1 200 ok",
-            "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-            "server: ruby",
-            "content-type: text/html; charset=iso-8859-1",
-            "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-  client.puts headers
-  client.puts output
-
-  puts ["Wrote this response:", headers, output].join("\n")
-
-  puts "Enter quit to stop server:"
-  closer = gets.chomp
-  break if closer == "quit"
 end
-
-
-
-
-puts "\nResponse complete, exiting."
---
+++ http_experiment_test.rb
@@ -0,0 +1,23 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './http_experiment'
+
+class HttpExperimentTest < Minitest::Test
+
+  def test_can_start_server
+    e = Experiment.new
+
+    assert e
+  end
+
+  def test_counter_counts
+  end
+
+  def test_it_
+
+
+
+
+end
